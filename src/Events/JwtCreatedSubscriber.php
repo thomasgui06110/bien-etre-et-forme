a8b7c05e3309539dd8a23d8e505d5de32f5a25eb
Thomasgui <guilhem.thomas@gmail.com>
2019-10-14 14:12:56 +0200

    Modification sur le cicle de vie de la requete

---
+++ src/Events/JwtCreatedSubscriber.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Events;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JwtCreatedSubscriber {
+
+    public function updateJwtData(JWTCreatedEvent $event) 
+    {
+        // 1. Recuperer utilisateur pour avoir firstanme et lastname
+        $user = $event->getUser();
+
+        // 2. Enrichir les data pour qu'elles contiennent ces données
+        $data = $event->getData();
+        $data['firstName'] = $user->getFirstName();
+        $data['lastName'] = $user->getLastName();
+
+        $event->setData($data);
+        //dd($event->getData());    
+    }
+}
